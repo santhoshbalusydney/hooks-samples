48767be5fbd23356c95d3916e2c84c6e46b075a4
Santhosh Balu <chenkarai@gmail.com>
2021-09-11 20:58:49 +1000

    upload useref tryout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useRef } from "react";
 import { useForm } from "./useForm";
-import { useFetch } from "./useFetch";
+import { Hello } from "./Hello";
 
 const App = () => {
   const [values, handleChange] = useForm({
@@ -8,17 +8,22 @@ const App = () => {
     password: "",
     firstName: ""
   });
-  const [count, setCount] = useState(0);
-  // const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
-  const { data, loading } = useFetch(`https://jsonplaceholder.typicode.com/todos/${count}`);
+  const inputRef = useRef();
+  const hello = useRef(() => console.log("hello"));
+
+  const [showHello, setShowHello] = useState(true);
 
   return (
     <div>
-      <div>{!data ? "loading..." : data}</div>
-      <div>count: {count}</div>
-      <button onClick={() => setCount(c => c + 1)}>increment</button>
       <>
-        <input name="email" value={values.email} onChange={handleChange} />
+        <button onClick={() => setShowHello(!showHello)}>toggle</button>
+        {showHello && <Hello />}
+        <input
+          ref={inputRef}
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
         <input
           name="firstName"
           placeholder="first name"
@@ -31,6 +36,14 @@ const App = () => {
           value={values.password}
           onChange={handleChange}
         />
+        <button
+          onClick={() => {
+            inputRef.current.focus();
+            hello.current();
+          }}
+        >
+          focus
+        </button>
       </>
     </div>
   );
