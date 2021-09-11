8332b56e23b9b0b0e0c6bfa066a6a7c1e998c11b
Santhosh Balu <chenkarai@gmail.com>
2021-09-11 21:38:02 +1000

    upload usereducer sample

--- src/App.js
+++ src/App.js
@@ -1,37 +1,54 @@
-import React, { useEffect, useState } from "react";
-import { useForm } from "./useForm";
-import { useFetch } from "./useFetch";
+import React, { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add-todo":
+      return {
+        todos: [...state.todos, { text: action.text, completed: false }],
+        todoCount: state.todoCount + 1
+      };
+    case "toggle-todo":
+      return {
+        todos: state.todos.map((t, idx) =>
+          idx === action.idx ? { ...t, completed: !t.completed } : t
+        ),
+        todoCount: state.todoCount
+      };
+    default:
+      return state;
+  }
+}
 
 const App = () => {
-  const [values, handleChange] = useForm({
-    email: "",
-    password: "",
-    firstName: ""
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+    todos: [],
+    todoCount: 0
   });
-  const [count, setCount] = useState(0);
-  // const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
-  const { data, loading } = useFetch(`https://jsonplaceholder.typicode.com/todos/${count}`);
+  const [text, setText] = useState();
 
   return (
     <div>
-      <div>{!data ? "loading..." : data}</div>
-      <div>count: {count}</div>
-      <button onClick={() => setCount(c => c + 1)}>increment</button>
-      <>
-        <input name="email" value={values.email} onChange={handleChange} />
-        <input
-          name="firstName"
-          placeholder="first name"
-          value={values.firstName}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-        />
-      </>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          dispatch({ type: "add-todo", text });
+          setText("");
+        }}
+      >
+        <input value={text} onChange={e => setText(e.target.value)} />
+      </form>
+      <div>number of todos: {todoCount}</div>
+      {todos.map((t, idx) => (
+        <div
+          key={t.text}
+          onClick={() => dispatch({ type: "toggle-todo", idx })}
+          style={{
+            textDecoration: t.completed ? "line-through" : ""
+          }}
+        >
+          {t.text}
+        </div>
+      ))}
     </div>
   );
 };
